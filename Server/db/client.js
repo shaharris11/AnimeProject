7b6bb2c213bb7e5d3523c9da235295cf6c050a97
shakira <shaharris11@yahoo.com>
2024-02-29 15:10:25 -0500

    update

--- Server/db/client.js
+++ Server/db/client.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg');
 
-const client = new Client(`postgres://localhost:3546/anime`);
+const client = new Client(`postgres://anime_xotu_user:DphHJK5DyG5x4tA9tnmSqdpJRLT4JFe5@dpg-cnge71ed3nmc739o4n0g-a/anime_xotu`);
 
 module.exports = client;
